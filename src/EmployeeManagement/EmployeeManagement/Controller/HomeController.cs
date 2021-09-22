9dc8ff2eabe9312e7d5e8fe7f244ae78b5f38a73
Anuj Jain <anujjain2882@gmail.com>
2021-09-23 00:39:13 +0530

    ASP.NET Core Model Binding

--- src/EmployeeManagement/EmployeeManagement/Controller/HomeController.cs
+++ src/EmployeeManagement/EmployeeManagement/Controller/HomeController.cs
@@ -35,9 +35,17 @@ namespace EmployeeManagement
             return View(homeDetailsViewModel);
         }
 
+        [HttpGet]
         public ViewResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Create(Employee employee)
+        {
+            Employee newEmployee = _employeeRepository.Add(employee);
+            return RedirectToAction("details", new { id = newEmployee.Id });
         }
     }
 }
