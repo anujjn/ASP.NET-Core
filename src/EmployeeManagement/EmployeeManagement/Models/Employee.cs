6ac21c46b6b356139caafae13aa23c0efae38c8a
Anuj Jain <anujjain2882@gmail.com>
2021-10-19 23:54:23 +0530

    Entity Framework Core

--- src/EmployeeManagement/EmployeeManagement/Models/Employee.cs
+++ src/EmployeeManagement/EmployeeManagement/Models/Employee.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace EmployeeManagement.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+
+        [Required, MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
         public string Name { get; set; }
+
+        [Display(Name = "Office Email")]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+            ErrorMessage = "Invalid email format")]
+        [Required(ErrorMessage = "Enter an Email")]
         public string Email { get; set; }
-        public Dept Department { get; set; }
+
+        [Required]
+        public Dept? Department { get; set; }
     }
 }
--- src/EmployeeManagement/EmployeeManagement/Startup.cs
+++ src/EmployeeManagement/EmployeeManagement/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,8 +28,9 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(_configuration.GetConnectionString("EmployeeDBConnection")));
             services.AddMvc(options => options.EnableEndpointRouting = false);
-            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+            services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
